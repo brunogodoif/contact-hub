67958381a319a9b0a61d8c46ffffade906aa5793
Bruno Godoi <brunofgodoi@outloo.com.br>
2024-03-11 20:01:44 -0300

    test utils - UUIDValidator

---
+++ src/test/java/br/com/brunogodoif/contacthub/uilts/validators/UuidValidatorSpec.groovy
@@ -0,0 +1,30 @@
+package br.com.brunogodoif.contacthub.uilts.validators
+
+import br.com.brunogodoif.contacthub.core.services.exceptions.InvalidParamUUIDTypeException
+import br.com.brunogodoif.contacthub.utils.validators.UuidValidator
+import spock.lang.Specification
+
+class UuidValidatorSpec extends Specification{
+    def "Validates UUID correctly"() {
+        given: "A valid UUID"
+        String validUuid = "123e4567-e89b-12d3-a456-426614174000"
+
+        when: "Checking if the UUID is valid"
+        boolean isValid = UuidValidator.isValidUUID(validUuid)
+
+        then: "The result should be true"
+        isValid == true
+        noExceptionThrown()
+    }
+
+    def "Invalidates an incorrect UUID"() {
+        given: "An invalid UUID"
+        String invalidUuid = "invalid-uuid"
+
+        when: "Checking if the UUID is valid"
+        UuidValidator.isValidUUID(invalidUuid)
+
+        then: "An InvalidParamUUIDTypeException should be thrown"
+        thrown(InvalidParamUUIDTypeException)
+    }
+}
